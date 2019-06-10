33bb759ea9802b2c52128f2e4e348f163165df47
zeta1999 <renoir42@yahoo.com>
2019-06-10 20:14:50 +0900

    some code

--- TestTransition01.py
+++ TestTransition01.py
@@ -4,86 +4,44 @@ from typing import Type, TypeVar, Generic, NewType, TYPE_CHECKING, overload, Any
 ctx: Transition.Context = Transition.Context()
 
 class A:
-    pass
+    def __repr__(self) -> str:
+        return "A"
 class B:
-    pass
+    def __repr__(self) -> str:
+        return "B"
 class C:
-    pass
+    def __repr__(self) -> str:
+        return "C"    
 class D:
-    pass
+    def __repr__(self) -> str:
+        return "D"
 class E:
-    pass
+    def __repr__(self) -> str:
+        return "E"
 class Q:
-    pass
-
-class TM(type):
-    @classmethod
-    def __class_getitem__(cls, item):
-        print("TM", str(cls), str(item))    
-        return str(cls)
-    @classmethod
-    def __getitem__(cls, item):
-        print("TM2", str(cls), str(item))  
-        def emu():
-            return str(cls)
-        return emu
+    def __repr__(self) -> str:
+        return "Q"
 
 T = TypeVar('T')
 U = TypeVar('U')
 V = TypeVar('V')
 W = TypeVar('W')
 
-class TR(Generic[T], metaclass = TM):
-    def id(self, x):
-        return x
-
-class XY(Generic[T,U]):
-    pass
-
 einop_AB_B: Callable[
-    [Transition.Tensor2[E, float, A, B], B], Transition.Tensor1[E, float, B]] = Transition.einop_2_1_((A(),B()), B())
+    [Transition.Tensor2[E, float, A, B], B], Transition.Tensor1[E, float, B]] = Transition.einop_2_1_(ctx, (A(),B()), B())
 
-#conv1dim = Transition.conv1d_dim_(A(), B(), D(), Transition._4(), Transition._3(), Transition._3(), Transition._3(), Transition._3()) 
-#reveal_type(conv1dim)
 conv1d : Callable[[Transition.Tensor3[E,float, A,B, C]], Transition.Tensor3[E,float, A, B, Q]] = Transition.conv1d_(Q(), float, ctx, C(), B(), B(), Transition._4(), Transition._3(), Transition._3(), Transition._3(), Transition._3()) 
-# reveal_type(conv1d)
 
 def example01(s: Transition.Session[E]):
     t1: Transition.Tensor1[E, float, A] = Transition.ones(s, float, A())
     t2: Transition.Tensor1[E, float, A] = Transition.ones(s, float, A())
-    #t3: Transition.Tensor1[E, float, A] = Transition.ones_(s, float, (B,), Transition.Tensor1[E, float, B])
-    Transition.update(t1, t2)
-    reveal_type(t1)
-    reveal_type(t2)
-    #reveal_type(t3)
+    Transition.update1(t1, t2)
+
     t3: Transition.Tensor2[E, float, A, B] = Transition.ones2(s, float, A(), B())
     t4: Transition.Tensor1[E, float, B] = Transition.einop_2_1(t3, B())
-    reveal_type(t4)
-    t5: Transition.Tensor1[E, float, B] = einop_AB_B(t3, C())
-    reveal_type(t5)
-    tr: TR[XY[A,B]] = TR[XY[A,B]]()
+
+    t5: Transition.Tensor1[E, float, B] = einop_AB_B(t3, B())
+
     t6: Transition.Tensor3[E, float, A, B, C] = Transition.ones3(s, float, A(), B(), C())
-    t7: Transition.Tensor3[E, float, A, C, Q] = conv1d(t6)
-    reveal_type(t7)
+    t7: Transition.Tensor3[E, float, A, B, Q] = conv1d(t6)
 
-tr2: TR[XY[A,C]] = TR[XY[A,C]]()
-
-### TODO: try to catch bad dim extraction BEFORE running the code
-"""
-class AddDim0(type):
-    @classmethod
-    def __class_getitem__(cls, item):
-        print(type(cls), cls.__dict__)
-        print(type(item), item.__dict__)
-        print("***getitem " + str(cls) + " " + str(item))
-        return f"{str(cls)}[int]"
-    @classmethod
-    def __getitem__(cls, item):
-        print(type(cls), cls.__dict__)
-        print(type(item), item.__dict__)
-        print("***getitem " + str(cls) + " " + str(item))
-        return f"{str(cls)}[float]"        
-
-class AddDim(Generic[T], metaclass = AddDim0):
-    pass
-    """
--- Transition.py
+++ Transition.py
@@ -25,10 +25,8 @@ X = TypeVar('X')
 Y = TypeVar('Y')
 Z = TypeVar('Z')
 
-class AbstractTensor:
-    pass
-
-# see https://keras.io/backend/#backend for inspiration
+# See https://keras.io/backend/#backend for inspiration
+# Note this is a 'do nothing' implementation and would have to be derived for a real case
 class Backend(Generic[E]):
     def __init__(self):
         self._epsilon : float = 1e-7
@@ -74,20 +72,20 @@ class Backend(Generic[E]):
         return x
     def exp(self, x: E) -> E:
         return x
-    def variable(self, value, dtype=None, name=None, constraint=None) -> E:
-        pass
-    def constant(self, value, dtype=None, shape=None, name=None) -> E:
-        pass
-    def placeholder(self, shape=None, ndim=None, dtype=None, sparse=False, name=None) -> E:
-        pass
-    def zeros(self, shape, dtype=None, name=None) -> E:
-        pass
-    def ones(self, shape, dtype=None, name=None) -> E:
-        pass
-    def random_uniform_variable(self, shape, low, high, dtype=None, name=None, seed=None) -> E:
-        pass
-    def random_normal_variable(self, shape, mean, scale, dtype=None, name=None, seed=None) -> E:
-        pass
+    def variable(self, s: E, value, dtype=None, name=None, constraint=None) -> E:
+        return s
+    def constant(self, s:E, value, dtype=None, shape=None, name=None) -> E:
+        return s
+    def placeholder(self, s:E, shape=None, ndim=None, dtype=None, sparse=False, name=None) -> E:
+        return s
+    def zeros(self, s: E, shape, dtype=None, name=None) -> E:
+        return s
+    def ones(self, s: E, shape, dtype=None, name=None) -> E:
+        return s
+    def random_uniform_variable(self, s: E, shape, low, high, dtype=None, name=None, seed=None) -> E:
+        return s
+    def random_normal_variable(self, s: E, shape, mean, scale, dtype=None, name=None, seed=None) -> E:
+        return s
     
 class Tensor1(Generic[E, F, T]):
     def __init__(self, b: Backend[E], f: Type[F], t: T):
@@ -102,12 +100,12 @@ class Tensor1(Generic[E, F, T]):
         return 1
     def get_ftype(self) -> Type[F]:
         pass
-    def get_dim1_type(self) -> Type[T]:
+    def get_dim1_type(self) -> T:
         pass
     def get_dim1(self) -> T:
         pass        
 class Tensor2(Generic[E, F, T, U]):
-    def __init__(self, b: Backend[E], f: Type[F], t: Type[T], u: Type[U]):
+    def __init__(self, b: Backend[E], f: Type[F], t: T, u: U):
         pass
     def get_backend(self) -> Backend[E]:
         pass
@@ -119,55 +117,97 @@ class Tensor2(Generic[E, F, T, U]):
         return 2
     def get_ftype(self) -> Type[F]:
         pass
-    def get_dim1_type(self) -> Type[T]:
+    def get_dim1_type(self) -> T:
         pass
-    def get_dim2_type(self) -> Type[U]:
+    def get_dim2_type(self) -> U:
         pass
+# Similarly, with order 3,4, ...        
 class Tensor3(Generic[E, F, T, U, V]):
-    pass
-class Tensor4(Generic[E, F, T, U, V, W]):
-    pass
+    def __init__(self, b: Backend[E], f: Type[F], t: T, u: U, v: V):
+        pass
+    def get_backend(self) -> Backend[E]:
+        pass
+    def get_handle(self) -> E:
+        pass
+    def set_handle(self, e: E):
+        pass
+    def get_order(self) -> int:
+        return 3
+    def get_ftype(self) -> Type[F]:
+        pass
+    def get_dim1_type(self) -> T:
+        pass
+    def get_dim2_type(self) -> U:
+        pass
+    def get_dim3_type(self) -> V:
+        pass        
+#class Tensor4(Generic[E, F, T, U, V, W]):
+#    pass
 
+# abstract dimensions A,B,C,D
 class A:
-    pass
+    def __repr__(self) -> str:
+        return "A"
 class B:
-    pass
+    def __repr__(self) -> str:
+        return "B"
 class C:
-    pass
+    def __repr__(self) -> str:
+        return "C"    
 class D:
-    pass
+    def __repr__(self) -> str:
+        return "D"
 
-class Succ(Generic[T]):
-    pass
 
+# classes that represent fixed dims (for use in dim formulas)
 class _0:
-    pass
-_1 = Succ[_0]
-_2 = Succ[_1]
-_3 = Succ[_2]
-_4 = Succ[_3]
-_5 = Succ[_4]
+    def __repr__(self)->str:
+        return "0"
+class _1:
+    def __repr__(self)->str:
+        return "1"
+class _2:
+    def __repr__(self)->str:
+        return "2"            
+class _3:
+    def __repr__(self)->str:
+        return "3"          
+class _4:
+    def __repr__(self)->str:
+        return "4"          
 
 class DimAdd(Generic[U,V]):
     def __init__(self, u:U, v: V):
         self._u = u
         self._v = v
+    def __repr__(self) -> str:
+        return "(" + self._u.__repr__() + "+" + self._v.__repr__() + ")"
 
 class DimMul(Generic[U,V]):
     def __init__(self, u:U, v: V):
         self._u = u
         self._v = v
+    def __repr__(self) -> str:
+        return "(" + self._u.__repr__() + "*" + self._v.__repr__() + ")"
 
 class DimSub(Generic[U,V]):
     def __init__(self, u:U, v: V):
         self._u = u
         self._v = v
+    def __repr__(self) -> str:
+        return "(" + self._u.__repr__() + "-" + self._v.__repr__() + ")"
 
 class DimDiv(Generic[U,V]):
     def __init__(self, u:U, v: V):
         self._u = u
         self._v = v
+    def __repr__(self) -> str:
+        return "(" + self._u.__repr__() + "/" + self._v.__repr__() + ")"
 
+class EqCert(Generic[T,U]):
+    pass
+class Inclusion1Cert(Generic[T,U,V]):
+    pass
 
 @dataclass
 class Equality:
@@ -175,20 +215,21 @@ class Equality:
     right: Any
 
 @dataclass
-class DimSumAST:
+class Inclusion:
     left: Any
-    right: Any    
-
-class EqCert(Generic[T,U]):
-    pass
+    right: Any
 
 class Session(Generic[E]):
     def __init__(self):
         self._backend = Backend[E]
+        dims : Dict[any, int] = {}
+        self._dims = {}
     ## set dim
+    def set_dim(self, a: Any, d: int):
+        self._dims[a] = d
     ## get dim
-    def get_dim(a: Any) -> int:
-        return 0
+    def get_dim(self, a: Any) -> int:
+        return self._dims[a]
     def get_backend(self) -> Backend[E]:
         return self._backend
     def set_backend(self, b: Backend[E]) -> 'Session[E]':
@@ -206,86 +247,41 @@ class Context:
         self.tocheck.append(Equality(d1,d2))
         # TODO check
         return EqCert[T,U]()
-    def check_equal(self, d1: Type[T], d2: Type[U]) -> EqCert[T,U]:
-        self.tocheck.append(Equality(d1,d2))
+    def check_in2_(self, d12: Tuple[T, U], d2: V):
+        print("check is in collection", d12, d2)
         # TODO check
-        return EqCert[T,U]()
-    # no
-    def cast(self, s: Session, t1: Tensor1[E, F, U], ttx:EqCert[U, T]) -> Tensor1[E, F, T]:
-        #self.check_equal(tx, t2)
-        pass # Tensor1[E, F,T]()    
+        self.tocheck.append(Inclusion(d12, d2))
+        return Inclusion1Cert[T,U,V]()
 
-## def conv1d(self, x:E, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros') -> E:
-## size type creator...
-
-#@overload
-#def update(t1: Tensor1[E,F,T], t2: Tensor1[E,F,T]) -> Tensor1[E,F,T]: ...
-#@overload
-#def update(t1: Tensor2[E,F,T,U], t2: Tensor2[E,F,T,U]) -> Tensor2[E,F,T,U]: ...
-
-#todo update 1/2/3/4
-def update(t1: Tensor1[E,F,T], t2: Tensor1[E,F,T]) -> Tensor1[E,F,T]:
+def update1(t1: Tensor1[E,F,T], t2: Tensor1[E,F,T]) -> Tensor1[E,F,T]:
     backend = t1.get_backend()
     t1_h = t1.get_handle()
     t2_h = t2.get_handle()
     rv_h = backend.update(t1_h, t2_h)
     o = t1.get_order()
     f = t1.get_ftype()
-    #if o == 1:
     rv = Tensor1(backend, f, t1.get_dim1())
     rv.set_handle(rv_h)
     return rv
-    #if o == 2:
-    #    rv = Tensor1(backend, f, t1.get_dim1_type(), t1.get_dim2_type())
-    #    rv.set_handle(rv_h)
-    #    return rv
-    #error("update / tensor size")
 
+# similarly we would add 2D, 3D, 4D versions
 
 def ones(s: Session[E], f: Type[F], x: T) -> Tensor1[E, F, T]:
     pass 
 def ones2(s: Session[E], f: Type[F], x: T, y:U) -> Tensor2[E, F, T, U]:
     pass
-def ones3(s: Session[E], f: Type[F], x: T, y:U, z: V) -> Tensor3[E, F, T, U, V]:
+def ones3(s: Session[E], f: Type[F], x: T, y:U, z: Z) -> Tensor3[E, F, T, U, Z]:
     pass
-
-## crap
-@overload
-def ones_(s: Session[E], f: Type[F], x: Tuple[Type[T]], ty: Type[Tensor1[E, F, T]]) -> Tensor1[E, F, T]: ...
-@overload
-def ones_(s: Session[E], f: Type[F], x: Tuple[Type[T], Type[U]], ty: Type[Tensor2[E, F, T, U]]) -> Tensor2[E, F, T, U]: ...
-
-def ones_(s, f, x, ty):
-    pass
-
-def ze_ones(s, f, x):
-    print(x)
-    backend = s.get_backend()
-    if (len(x) == 1):
-        rv = Tensor1(backend, f, x[0])
-        h = backend.ones(shape = (s.get_dim(x[0])), dtype = f)
-        rv.set_handle(h)
-        return rv
-    if (len(x) == 2):
-        rv = Tensor2(backend, f, x[0], x[1])
-        h = backend.ones(shape = (s.get_dim(x[0]), s.get_dim(x[1])), dtype = f)
-        rv.set_handle(h)
-        return rv
-    error("ones / shape size")
 
 def einop_2_1(t: Tensor2[E, F, T, U], tgt: V) -> Tensor1[E,F,V]:
     pass
 
-#non
-class Einop_2_1(Generic[T, V, U]):
-    pass
-
-def einop_2_1_(src: Tuple[T, U], tgt: V) -> Callable[[Tensor2[E, F, T, U], V], Tensor1[E,F,V]]:
-    print("todo check:", src, tgt)
+def einop_2_1_(c: Context, src: Tuple[T, U], tgt: V) -> Callable[[Tensor2[E, F, T, U], V], Tensor1[E,F,V]]:
+    c.check_in2_(src, tgt)
     return lambda t, tg: einop_2_1(t, tg)
 
 def cast1_(c: Context, src: T, tgt: V) -> Callable[[Tensor1[E,F,T]], Tensor1[E,F,V]]:
-    print("todo check:", src, tgt)
+    c.check_equal_(src, tgt)
     def rv(t1: Tensor1[E,F,T]):
         backend = t1.get_backend()
         r = Tensor1[E,F,V](backend, t1.get_ftype(), tgt)
@@ -294,7 +290,7 @@ def cast1_(c: Context, src: T, tgt: V) -> Callable[[Tensor1[E,F,T]], Tensor1[E,F
         return r
     return rv
 
-   # https://pytorch.org/docs/stable/nn.html#torch.nn.Conv1d
+    # https://pytorch.org/docs/stable/nn.html#torch.nn.Conv1d
     # input (N, C in, L in)
     # output (N, C out, L out)
     # where L out = round lower( (l in + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1 )
@@ -330,71 +326,7 @@ def conv1d_(tgt: Q,
     groups : Z, 
     bias=True, 
     padding_mode='zeros') -> Callable[[Tensor3[E,F, R,T, S]], Tensor3[E,F, R, U, Q]]:
-    pass
-
-#no
-def conv1d(t1: Tensor3[E,F, R,T, S],
-    in_channels : T, 
-    out_channels : U, 
-    kernel_size : V, 
-    stride : W, 
-    padding : X, 
-    dilation : Y, 
-    groups : Z, 
-    bias=True, 
-    padding_mode='zeros') -> Tensor3[E,F, R, U, DimDiv[DimSub[DimSub[DimAdd[S, DimMul[_2,X]], DimMul[Y, DimSub[V, _1]]],_1],DimAdd[W, _1]]] :
-    pass
-
-"""
-    def update(self, x: E, new_x: E) -> E:
-        return x
-    def update_add(self, x: E, increment: E) -> E:
-        return x
-    def update_sub(self, x: E, decrement: E) -> E:
-        return x
-    def dot(self, x: E, y: E) -> E:
-        return x
-    def einop(self, x: E, spec: str) -> E:
-        return x
-    def einop2(self, x:E, y:E, spec: str) -> E:
-        return x
-    def einop3(self, x:E, y:E, z:E, spec: str) -> E:
-        return x
-    # https://pytorch.org/docs/stable/nn.html#torch.nn.Conv1d
-    # input (N, C in, L in)
-    # output (N, C out, L out)
-    # where L out = round lower( (l in + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1 )
-    def conv1d(self, x:E, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros') -> E:
-        return x
-    def exp(self, x: E) -> E:
-        return x
-    def variable(self, value, dtype=None, name=None, constraint=None) -> E:
-        return E()
-    def constant(self, value, dtype=None, shape=None, name=None) -> E:
-        return E()
-    def placeholder(self, shape=None, ndim=None, dtype=None, sparse=False, name=None) -> E:
-        return E()
-    def zeros(self, shape, dtype=None, name=None) -> E:
-        return E()
-    def ones(self, shape, dtype=None, name=None) -> E:
-        return E()
-    def random_uniform_variable(self, shape, low, high, dtype=None, name=None, seed=None) -> E:
-        return E()
-    def random_normal_variable(self, shape, mean, scale, dtype=None, name=None, seed=None) -> E:
-        return E()
-"""
-
-"""
-class EE:
-    pass
-
-def example01(s: Session[EE]):
-    t1: Tensor1[EE, float, A] = ones(s, float, A())
-    t2: Tensor1[EE, float, B] = ones(s, float, B())
-    t3: Tensor1[EE, float, A] = ones_(s, float, (B,), Tensor1[EE, float, B])
-    update(t1, t2)
-    reveal_type(t1)
-    reveal_type(t2)
-    reveal_type(t3)
-
-"""
+    conv1d_dim_(tgt, ctx, l_in, in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias, padding_mode)
+    def g(x):
+        pass
+    return g # type: ignore
